f65789f9bcbbe8d38c4b0c599dcd72c1e5179df0
ishaan007 <contact.ishaanarora@gmail.com>
2019-07-28 22:23:51 +0530

    changed requirements.txt13

--- app.py
+++ app.py
@@ -63,4 +63,4 @@ def index():
     return (render_template('index.html'))
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0',use_reloader=False)
+    app.run(debug=True,use_reloader=False)
